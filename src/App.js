995b7d37986fdab8e1a084aa48912ab992c03e9c
Tahseen1999 <65603430+Tahseen1999@users.noreply.github.com>
2020-06-03 14:26:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Website</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet magna sit amet dui iaculis gravida eleifend non est. Aliquam erat volutpat. Donec vehicula felis ligula, quis pellentesque felis blandit sit amet. Phasellus diam lectus, cursus sit amet dapibus vitae, pharetra id sapien. Sed viverra, elit et efficitur sollicitudin, ante nulla cursus nulla, ac auctor mi tellus tempor velit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tortor nunc, ultrice</p>
+      <img src="https://tse1.mm.bing.net/th?id=OIP.zsTrKewVx7N6A-XE0BoOkAHaHM&pid=Api&P=0&w=178&h=174" width="300" height="250"/>
+      <p>Suspendisse dapibus augue sed dolor sodales euismod. Vivamus blandit consectetur velit sit amet scelerisque. Fusce semper nulla dui, et sagittis neque varius eget. In eu tellus in dui mollis auctor sit amet vitae mauris. Curabitur magna ligula, egestas a justo ac, ullamcorper vestibulum felis. Duis posuere volutpat metus, in scelerisque nisl porta ac. Vivamus lobortis ipsum id libero porta congue. Maecenas sagittis, nisl eget sollicitudin commodo, justo sapien mollis dolor, sed lacinia mauris metus ut est. Morbi dictum massa sed lorem commodo, ac convallis eros ultrices. Aenean nec </p>
+      <a href="https://www.google.co.in/">Google me</a>
+    </div>
+  );
+}
+
+export default App;
