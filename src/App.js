45c327f3b7a797822149de7555a3e7ebbb0270d8
Tahseen1999 <65603430+Tahseen1999@users.noreply.github.com>
2020-06-04 12:43:16 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,62 @@
-import React from 'react';
+import React,{Component}from 'react';
 import './App.css';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Website</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet magna sit amet dui iaculis gravida eleifend non est. Aliquam erat volutpat. Donec vehicula felis ligula, quis pellentesque felis blandit sit amet. Phasellus diam lectus, cursus sit amet dapibus vitae, pharetra id sapien. Sed viverra, elit et efficitur sollicitudin, ante nulla cursus nulla, ac auctor mi tellus tempor velit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tortor nunc, ultrice</p>
-      <img src="https://tse1.mm.bing.net/th?id=OIP.zsTrKewVx7N6A-XE0BoOkAHaHM&pid=Api&P=0&w=178&h=174" width="300" height="250"/>
-      <p>Suspendisse dapibus augue sed dolor sodales euismod. Vivamus blandit consectetur velit sit amet scelerisque. Fusce semper nulla dui, et sagittis neque varius eget. In eu tellus in dui mollis auctor sit amet vitae mauris. Curabitur magna ligula, egestas a justo ac, ullamcorper vestibulum felis. Duis posuere volutpat metus, in scelerisque nisl porta ac. Vivamus lobortis ipsum id libero porta congue. Maecenas sagittis, nisl eget sollicitudin commodo, justo sapien mollis dolor, sed lacinia mauris metus ut est. Morbi dictum massa sed lorem commodo, ac convallis eros ultrices. Aenean nec </p>
-      <a href="https://www.google.co.in/">Google me</a>
-    </div>
-  );
+constructor(props){
+  super(props);
+
+  this.state={
+    FirstName:"Enter your first name",
+    LastName:"Enter your Last name",
+    email:"Enter your email id",
+    phone_no:"987879787",
+    message:"Enter message"
+  }
+}
+handlechange=(event)=>{
+  this.setState({[event.target.name]:event.target.value})
+
+}
+handlesubmit=(event)=>{
+  alert(`my name is ${this.state.FirstName} ${this.state.LastName}
+      my email id is ${this.state.email}
+      my mobile number is ${this.state.phone_no}
+      my message to your company is ${this.state.message}
+  `);
+  event.preventDefault();
+}
+  render(){
+    return (
+      <div className="App">
+        <h1><u>Register your store</u></h1><br/>
+        <form onSubmit={this.handlesubmit}>
+        <label>First Name</label><br/>
+        <input type="text" name="FirstName" value={this.state.FirstName}
+                onChange={this.handlechange}/>
+        <br/>
+        <label>Last Name</label><br/>
+        <input type="text" name="LastName" value={this.state.LastName}
+                onChange={this.handlechange}/>
+        <br/>
+        <label>Email</label><br/>
+        <input type="email" name="email" value={this.state.email}
+                onChange={this.handlechange}/>
+        <br/>
+        <label>Phone number</label><br/>
+        <input type="number" name="phone_no" value={this.state.phone_no}
+                onChange={this.handlechange}/>
+        <br/>
+        <label>Message</label><br/>
+        <textarea name="message" value={this.state.message}
+                onChange={this.handlechange}/><br/><br/>
+
+      <input type="submit" />
+
+        </form>
+      </div>
+  
+    );
+  }
 }
 
 export default App;
